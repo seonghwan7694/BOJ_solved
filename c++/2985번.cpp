cb51fa68d6d1ec62a4da85ecd1ae5dfe9cf50ebc
seonghwan7694 <seonghwan7694@gmail.com>
2022-10-21 16:21:42 +0900

    Create 2985번.cpp

---
+++ c++/2985번.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int a, b, c;
+  cin >> a >> b >> c;
+  if(a + b == c){ cout << a << "+" << b << "=" << c; }
+  else if(a - b == c){ cout << a << "-" << b << "=" << c; }
+  else if(a / b == c){ cout << a << "/" << b << "=" << c; }
+  else if(a * b == c){ cout << a << "*" << b << "=" << c; }
+  else if(a == b + c){ cout << a << "=" << b << "+" << c; }
+  else if(a == b - c){ cout << a << "=" << b << "-" << c; }
+  else if(a == b / c){ cout << a << "=" << b << "/" << c; }
+  else if(a == b * c){ cout << a << "=" << b << "*" << c; }
+
+
+  return 0;
+}
