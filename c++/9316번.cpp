aeab36bd4e4d6a4823800f922cb5962dad39b390
seonghwan7694 <seonghwan7694@gmail.com>
2023-04-02 01:39:45 +0000

    skip

---
+++ c++/9316번.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    int n; cin >> n;
+    for(int i = 1; i <= n; i++){
+        cout << "Hello World, Judge " << i << "!" << "\n";
+    }
+
+    return 0;
+}
