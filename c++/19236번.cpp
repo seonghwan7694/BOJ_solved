875e66a94590e23d9b1c1c8f07359acdacbf89a6
seonghwan7694 <seonghwan7694@gmail.com>
2023-05-13 18:45:52 +0900

    give up..

--- c++/19236번.cpp
+++ c++/19236번.cpp
@@ -9,27 +9,23 @@ pair<int, int> dir[9] = {{0, 0}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}, {1, 0}, {1
 
 array<array<pair<int, int>, 4>, 4> move_fish(array<array<pair<int, int>, 4>, 4> map, tuple<int, int, int> shark){
     auto [shark_x, shark_y, shark_dir] = shark;
-
+    
     for(int k = 1; k <= 16; k++){
         for(int i = 0; i < 4; i++){
             for(int j = 0; j < 4; j++){
-                if(map[i][j].first == k){
-                    int fish_nx, fish_ny, fish_dir = map[i][j].second;
-                    int cnt = 0;
-                    while(true){
-                        fish_nx = i + dir[fish_dir].first;
-                        fish_ny = j + dir[fish_dir].second;
-                        if(fish_nx < 0 or fish_nx >= 4 or fish_ny < 0 or fish_ny >= 4 or make_pair(fish_nx, fish_ny) == make_pair(shark_x, shark_y)){
-                            cnt++, fish_dir++;
-                            if(fish_dir > 8) fish_dir = 1;
-                            if(cnt > 9) break;
-                            continue;
-                        }
-                        break;
+                if(map[i][j].first != k) continue;
+                int fish_x = i, fish_y = j, fish_dir = map[i][j].second;
+                for(int i = 0; i < 8; i++){
+                    int fish_nx = fish_x + dir[fish_dir].first;
+                    int fish_ny = fish_y + dir[fish_dir].second;
+                    if(fish_nx < 0 or fish_nx >= 4 or fish_ny < 0 or fish_ny >= 4 or (fish_nx == shark_x and fish_ny == shark_y)){
+                        fish_dir++;
+                        if(fish_dir == 9) fish_dir = 1;
+                        continue;
                     }
-                    if(cnt > 9) break;
-                    map[i][j] = make_pair(map[i][j].first, fish_dir);
-                    swap(map[i][j], map[fish_nx][fish_ny]);
+                    map[fish_x][fish_y].second = fish_dir;
+                    swap(map[fish_x][fish_y], map[fish_nx][fish_ny]);
+                    break;
                 }
             }
         }
@@ -38,21 +34,21 @@ array<array<pair<int, int>, 4>, 4> move_fish(array<array<pair<int, int>, 4>, 4> 
 }
 
 void eat_fish(array<array<pair<int, int>, 4>, 4> map, tuple<int, int, int> shark, int score){
-    auto &[shark_x, shark_y, shark_dir] = shark;
+    auto [shark_x, shark_y, shark_dir] = shark;
 
     score += map[shark_x][shark_y].first;
-    shark_dir = map[shark_x][shark_y].second;
     MAX_SCORE = max(score, MAX_SCORE);
     map[shark_x][shark_y] = make_pair(0, 0);
 
-    auto new_map = move_fish(map, shark);
+    auto new_map = move_fish(map, make_tuple(shark_x, shark_y, shark_dir));
 
     for(int i = 1; i <= 3; i++){
         int x = shark_x + dir[shark_dir].first * i;
         int y = shark_y + dir[shark_dir].second * i;
         if(x < 0 or x >= 4 or y < 0 or y >= 4) continue;
-        if(map[x][y] == make_pair(0, 0)) continue;
-        eat_fish(new_map, make_tuple(x, y, shark_dir), score);
+        if(new_map[x][y].first == 0) continue;
+        auto nnew_map = new_map;
+        eat_fish(nnew_map, make_tuple(x, y, map[x][y].second), score);
     }
     return;
 }
@@ -66,7 +62,7 @@ int main(){
             map[i][j] = make_pair(a, b);
         }
     }
-    eat_fish(map, make_tuple(0, 0, 0), 0);
+    eat_fish(map, make_tuple(0, 0, map[0][0].second), 0);
     cout << MAX_SCORE << "\n";
     return 0;
 }
