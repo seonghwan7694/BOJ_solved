d4fb68cc79b58dee023213c601f37fec7fec1919
seonghwan7694 <seonghwan7694@gmail.com>
2023-09-13 18:39:45 +0900

    hrad

---
+++ c++/11877번.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lld long long int
+lld N, X;
+lld H, V;
+int main(){
+    ios::sync_with_stdio(false);
+    cin >> N >> X;
+// arrange h{1 ... N} -> capacity of histgram = exactly 'X'
+// 히스토그램의 용량 = 히스토그램에 물을 넘치지 않게 담았을 때 최대한 담을 수 있는 물의 양
+
+
+    return 0;
+}
