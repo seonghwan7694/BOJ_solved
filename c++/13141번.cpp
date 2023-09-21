83c64f91ea7cb483a62b8754b278c2a7722f92cc
seonghwan7694 <seonghwan7694@gmail.com>
2023-09-21 15:05:52 +0900

    create 13141

---
+++ c++/13141번.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+pair<int, int> graph[202][202];
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin >> N >> M;
+    for(int i = 0, S, E, L; i < M; i++){
+        cin >> S >> E >> L;
+
+    }
+
+
+    return 0;
+}
