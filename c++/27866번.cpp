28e780fa8048fb120ee4cb641e1bd14cd3d7fd6f
seonghwan7694 <seonghwan7694@gmail.com>
2023-09-21 15:13:02 +0900

    s

---
+++ c++/27866번.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    string str;
+    int n;
+    cin >> str >> n;
+    cout << str[n] << "\n";
+
+    return 0;
+}
