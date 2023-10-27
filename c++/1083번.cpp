a479f8452c7fb84cf593e9bd80c3ece502d3aae8
seonghwan7694 <seonghwan7694@gmail.com>
2023-10-27 18:33:45 +0900

    solved 1083

--- c++/1083번.cpp
+++ c++/1083번.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -11,18 +12,19 @@ int main(){
         cin >> A[i];
     }
     cin >> S;
-
-    for(int i = 0; i < N; i++){
-        for(int j = i + 1, k = i; j < N; j++, k++){
-            if(A[k] < A[j]){
-                swap(A[k], A[j]);
-                if(--S == 0){
-                    for(auto &x : A) cout << x << " ";
-                    cout << "\n";
-                    return 0;
-                }
-            }
+    int idx = 0;
+    while(S > 0){
+        if(idx == N) break;
+        vector<int>::iterator max_iter = max_element(A.begin() + idx, (A.begin() + idx + S + 1 > A.end()) ? A.end() : A.begin() + idx + S + 1);
+        if(max_iter == A.begin() + idx){
+            idx++;
+            continue;
         }
+        int max_val = *max_iter;
+        S -= max_iter - (A.begin() + idx);
+        A.erase(max_iter);
+        A.insert(A.begin() + idx, max_val);
+        idx++;
     }
     for(auto &x : A) cout << x << " ";
     cout << "\n";
